35a5c8ff8e7d960954a66dfa09e3fbf1eba8c99f
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-03-16 03:55:29 -0400

    added randomness

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 var colors = [
-  "rgb(255,0,0)",
-  "rgb(255,255,0)",
-  "rgb(0,255,0)",
-  "rgb(0,255,255)",
-  "rgb(0,0,255)",
-  "rgb(255,0,255)"
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
 ];
 console.log("hey");
 var squares = document.getElementsByClassName("square");
-console.log(squares.length);
-console.log("hey");
+var pckcolor = pickcolor();
+var msg = document.querySelector("#message");
+var res = document.getElementById("res");
+res.textContent = pckcolor;
+//console.log(squares.length);
+//console.log("hey");
 for (var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener("click", function() {
     var clickedColor = this.style.backgroundColor;
-    //console.log(clickedColor);
-    var ans = document.getElementById("res");
+    console.log(clickedColor);
     //console.log(ans.innerText);
-    if (clickedColor === ans.textContent) {
-      var msg = document.querySelector("#message");
+    console.log(clickedColor + " " + pckcolor);
+
+    if (clickedColor === pckcolor) {
+      //console.log("DSSSSSSSSSSSSSSSSS");
       msg.textContent = "Correct";
       changeColor(clickedColor);
     } else {
@@ -31,4 +36,10 @@ function changeColor(color) {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = color;
   }
+}
+
+function pickcolor(color) {
+  var x = Math.floor(Math.random() * colors.length);
+  //console.log(colors[x]);
+  return colors[x];
 }
